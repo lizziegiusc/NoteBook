6c59f21c4fa8e4b61df89d488af874e36330bf52
HP <HP@LAPTOP-HPNG6RJ5>
2019-09-25 23:00:32 -0700

    Solucion completa

---
+++ mvcPhone/mvcPhone/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace mvcPhone.Models
+{
+    public class DataContext :DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcPhone.Phone> Phones { get; set; }
+    }
+}
---
+++ mvcPhone/mvcPhone/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcPhone
+{
+    public enum TypeContact
+    {
+        PhoneNumber,
+        Email,
+        Facebook,
+        Twitter,
+        LinkedIN
+
+    }
+    public class Phone
+    {
+        [Key]
+        public int PhoneID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int My { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+        
+    }
+}
