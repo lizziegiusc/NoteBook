cbc6f0957ddd05027180f070d201f5c32e7c93d7
HP <HP@LAPTOP-HPNG6RJ5>
2019-09-26 17:06:56 -0700

    Web Api

---
+++ mvcPhone/apiPhone/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace apiPhone.Models
+{
+    using System.Data.Entity;
+    public class DataContext :DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
